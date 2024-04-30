9a8191f7be7ff0d8066ced6779cdb894f4d2f4bc
yeinnner <yeinnerortega@gmail.com>
2024-04-29 19:37:43 -0500

    Creacion de recursos y users

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    firstName: string;
+    @Column()
+    lastName: string;
+    @Column()
+    Address: string;
+    @Column()
+    phonenumber: string;
+    @Column()
+    email: string;
+    @Column({ default: true })
+    isActive: boolean;
+}
