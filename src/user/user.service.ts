d170d601d6a87c6884227a793bed3a2eae8dcbbe
yeinnner <yeinnerortega@gmail.com>
2024-04-29 22:35:11 -0500

    Creando Repository para user, y metodos POST, GET ALL

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User) 
+    private userRepository: Repository<User>
+  ){}  
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.userRepository.save(createUserDto);
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.userRepository.find();
   }
 
   findOne(id: number) {
